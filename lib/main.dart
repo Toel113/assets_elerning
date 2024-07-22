c63edea30a9fa51533fa03cd36ddf73702904baa
Toel113 <toelzx113@gmail.com>
2024-07-22 17:27:19 +0700

    Hive

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,17 @@
 import 'package:assets_elerning/LoadingPage.dart';
 import 'package:assets_elerning/api/firebase.dart';
 import 'package:flutter/material.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await initializeFirebase();
+  await Hive.initFlutter();
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
 
   @override
   Widget build(BuildContext context) {
